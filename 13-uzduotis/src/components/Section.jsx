4e556ee1328152b8191c512c5598fd5114f9296f
D3JS <Darius.alksnis@gmail.com>
2024-11-20 10:26:39 +0200

    add react 15

--- 13-uzduotis/src/components/Section.jsx
+++ 13-uzduotis/src/components/Section.jsx
@@ -13,14 +13,14 @@ export default function Section() {
     <div className="card w-25">
       <div className="card-body">
         <h5 className>
-          Task is {lightTheme ? "not done" : "done"}
+          Task is {lightTheme ? "done" : "not done"}
         </h5>
         <p className="card-text">
           Some quick example text to build on the card title and make up the bulk
           of the card's content.
         </p>
-        <button onClick={changeTheme} type="button" className={lightTheme ? "bg-danger" : "bg-success"}>
-          <p className="text-white">{lightTheme ? "Mark as done" : "done"}</p>
+        <button onClick={changeTheme} type="button" className={lightTheme ? "bg-success" : "bg-danger"}>
+          <p className="text-white">{lightTheme ? "done" : "Mark as done"}</p>
         </button>
       </div>
     </div>
---
+++ 15-uzduotis/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import PostContent from "./components/PostContent";
+import PostsList from "./components/PostsList";
+
+function App() {
+  return (
+    <div>
+      <PostsList />
+      
+    </div>
+  );
+}
+
+export default App;
