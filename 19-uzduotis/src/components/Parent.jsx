17d1f6a8fa0365b6f2960bb1f83507c38edb7c2d
D3JS <Darius.alksnis@gmail.com>
2024-11-25 11:34:02 +0200

    add react 21

--- 19-uzduotis/src/components/Parent.jsx
+++ 19-uzduotis/src/components/Parent.jsx
@@ -1,24 +1,17 @@
-import { useState } from 'react'
-import DisplayCounter from './DisplayCounter'
-import IncrementButton from './IncrementButton';
-
-
+import { useState } from "react";
+import DisplayCounter from "./DisplayCounter";
+import IncrementButton from "./IncrementButton";
 
 export default function Parent() {
+  const [count, setCount] = useState(0);
 
-    const [count, setCount] = useState(0);
-
-
-    return (
-
-
-        <div>
-            <h1>Counter App</h1>
-            <DisplayCounter countValue = {count}/>
-            <IncrementButton 
-            total ={setCount}
-            increment = {count}
-            />       
-        </div>
-    );
+  return (
+    <div>
+      <h1>Counter App</h1>
+      <DisplayCounter countValue={count} />
+      <IncrementButton 
+      total={setCount} 
+      increment={count} />
+    </div>
+  );
 }
