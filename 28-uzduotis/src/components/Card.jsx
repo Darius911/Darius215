62f360427502b7435ddd4803192894b709927b15
D3JS <Darius.alksnis@gmail.com>
2024-11-27 11:56:51 +0200

    add react 28

---
+++ 28-uzduotis/src/components/Card.jsx
@@ -0,0 +1,68 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router";
+
+export default function Card() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/users");
+        const data = await response.json();
+
+        setData(data);
+        setLoading(false);
+      } catch (error) {
+        setError(error.message);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
+
+  return (
+    <>
+      {data.map((post) => {
+        return (
+          <div key={post.id} className="col-lg-4">
+            <img className="bd-placeholder-img rounded-circle" width="140" height="140" role="img"  src={post.avatar_url} alt="" />
+            <h2 className="fw-normal">{post.username}</h2>
+            
+            <p>
+            <Link className="btn btn-secondary" to={`/posts/${post.id}`}>view detailds</Link>
+            </p>
+          </div>
+        );
+      })}
+    </>
+  );
+}
+
+<svg
+              className="bd-placeholder-img rounded-circle"
+              width="140"
+              height="140"
+              xmlns="http://www.w3.org/2000/svg"
+              role="img"
+              aria-label="Placeholder"
+              preserveAspectRatio="xMidYMid slice"
+              focusable="false"
+            >
+              <title>Placeholder</title>
+              <rect
+                width="100%"
+                height="100%"
+                fill="var(--bs-secondary-color)"
+              ></rect>
+            </svg>
